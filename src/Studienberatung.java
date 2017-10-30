1f5abcf87053aeae08682ff4f3f6b450096b7c7b
jenifer.prochnow <jeniferprochnow@web.de>
2017-10-30 14:36:44 +0100

    Bist du wissbegierig? implementiert

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -7,5 +7,17 @@ public class Studienberatung {
 		
 		System.out.println("Hast du Abitur?");
 		String antwort = scan.nextLine();
+		
+		if (antwort.equalsIgnoreCase("j")) {
+			System.out.println("Bist du wissbegierig?");
+			antwort = scan.nextLine();
+			if(antwort.equalsIgnoreCase("j")) {
+				System.out.println("Auf jeden Fall studieren");
+			}
+			
+			else if (antwort.equalsIgnoreCase("n")) {
+				System.out.println("Nicht studieren");
+			}
+		}
 	}
 }
