2635ba123750c678709fb8818cf58de71977d369
jenifer.prochnow <jeniferprochnow@web.de>
2017-10-30 14:57:22 +0100

    Hast du eine Berufsausbildung? implementiert

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -25,8 +25,15 @@ public class Studienberatung {
 			 System.out.println("Hast du Fachabitur?");
 			 antwort = scan.nextLine();
 			 if (antwort.equalsIgnoreCase("j")){
-					
-				} 
+				System.out.println("Hast du eine Berufsausbildung?");
+				antwort = scan.nextLine();
+				if (antwort.equalsIgnoreCase("j")) {
+					System.out.println("Du kannst ja später noch studieren");
+				}
+				else if (antwort.equalsIgnoreCase("n")) {
+					System.out.println("Besser noch studieren");
+				}
+			} 
 			 else if (antwort.equalsIgnoreCase("n")){
 				 System.out.println("Nicht studieren");
 			 } 
