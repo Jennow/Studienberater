afa81d18ef2d1b7d9586b72d3bb219098ea4dfc6
jenifer.prochnow <jeniferprochnow@web.de>
2017-10-30 14:28:24 +0100

    Hast du Fachabitur? implementiert

--- src/Studienberatung.java
+++ src/Studienberatung.java
@@ -7,5 +7,18 @@ public class Studienberatung {
 		
 		System.out.println("Hast du Abitur?");
 		String antwort = scan.nextLine();
+		if (antwort.equalsIgnoreCase("j")){
+			
+		} 
+		else if (antwort.equalsIgnoreCase("n")){
+			 System.out.println("Hast du Fachabitur?");
+			 antwort = scan.nextLine();
+			 if (antwort.equalsIgnoreCase("j")){
+					
+				} 
+			 else if (antwort.equalsIgnoreCase("n")){
+				 System.out.println("Nicht studieren");
+			 } 
+			} 
 	}
 }
